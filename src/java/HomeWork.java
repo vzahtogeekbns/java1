d771024d681eb3176e1aebf807beee36730de30c
Mikhail <levinmk23@gmail.com>
2020-01-14 22:15:21 +0300

    HomeWork tasks

---
+++ src/java/HomeWork.java
@@ -0,0 +1,35 @@
+public class HomeWork {
+    // TODO: 14.01.2020
+    /*
+    * Реализуйте методы данного класса
+    * */
+
+    public int task3(int a, int b, int c, int d) {
+        // TODO: 14.01.2020
+        return 0;
+    }
+
+    public boolean task4(int a, int b) {
+        // TODO: 14.01.2020
+        return false;
+    }
+
+    public void task5(int number) {
+        // TODO: 14.01.2020
+    }
+
+    public boolean task6(int val) {
+        // TODO: 14.01.2020
+        return false;
+    }
+
+    public void task7(String name) {
+        // TODO: 14.01.2020
+    }
+
+    public boolean isLeapYear(int year) {
+        // TODO: 14.01.2020 task8 *
+        return false;
+    }
+
+}
