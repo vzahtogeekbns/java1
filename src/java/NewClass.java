5ed5a8c2a0f1d9dee33ecab011411540499da4fd
Mikhail <levinmk23@gmail.com>
2020-01-17 19:36:10 +0300

    pull

--- src/java/NewClass.java
+++ src/java/NewClass.java
@@ -2,5 +2,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("world");
+        System.out.println("pull");
     }
 }
