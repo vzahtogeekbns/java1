9e350355e408211c7ee616664afe37bf70bf6b6b
Mikhail <levinmk23@gmail.com>
2020-01-20 23:12:55 +0300

    lesson3 HomeWork

---
+++ src/java/lesson3/homework/HomeWork.java
@@ -0,0 +1,19 @@
+package lesson3.homework;
+
+import java.util.Random;
+
+public class HomeWork {
+
+    public void binarySearchGame() {
+        int secretValue = new Random().nextInt(100); // компьютер загадывает число
+        // TODO: 20.01.2020
+        //мы печатаем в консоль наши предположения (в цикле), компютер отвечает > , < или =
+        //на каждый наш вопрос
+        //если задано более 7 вопросов и не угадано значение, мы проиграли
+        //если компютер вывел =, мы выиграли
+    }
+
+    public void wordsGame() {
+        // TODO: 20.01.2020  
+    }
+}
---
+++ src/test/TestHomeWork3.java
@@ -0,0 +1,7 @@
+import lesson3.homework.HomeWork;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestHomeWork3 {
+
+}
