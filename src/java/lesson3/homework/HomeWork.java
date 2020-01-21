2f2b4adc21399530d12ab9eb992489433ba62d9c
levinMK23 <levinmk23@gmail.com>
2020-01-21 15:35:36 +0300

    Added test for HomeWork

--- src/java/lesson3/homework/HomeWork.java
+++ src/java/lesson3/homework/HomeWork.java
@@ -1,8 +1,40 @@
 package lesson3.homework;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class HomeWork {
+
+    /* example:
+    * input: Mama mila ramu, Ramu mila mama!!!, [mama, ramu]
+    * output: 4
+    * use regex, split, lowerCase, replace methods
+    * */
+    public int countOfWordsFromDictionaryInString(String input, String [] dictionary) {
+        //посчитать все слова из словаря в input
+        //если одно слово встречается 5 раз его нужно посчитать 5 раз
+        // TODO: 1/21/2020
+        return 0;
+    }
+    /* example:
+     * input: [1,0,3,17,2,7,14,1,1,7], K = 6
+     * output: 3
+     * use sort
+     * */
+    public int kOrderValue(int [] array, int k) {
+        //метод должен вернуть К по порядку элемент массива
+        return 0;
+    }
+    /* example:
+     * input: m.levin.main@mailg.spb.com
+     * output: true
+     *
+     * use matches
+     * */
+    public boolean isEmail(String input) {
+        // TODO: 1/21/2020
+        return false;
+    }
 
     public void binarySearchGame() {
         int secretValue = new Random().nextInt(100); // компьютер загадывает число
--- src/test/TestHomeWork3.java
+++ src/test/TestHomeWork3.java
@@ -1,7 +1,47 @@
 import lesson3.homework.HomeWork;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestHomeWork3 {
 
+    HomeWork work;
+
+    @Before
+    public void init() {
+        work = new HomeWork();
+    }
+
+    @Test
+    public void testEmail() {
+        Assert.assertTrue(work.isEmail("email@email.com"));
+        Assert.assertTrue(work.isEmail("m.k.pupkin@host.zone.com"));
+        Assert.assertTrue(work.isEmail("m.k.pupkin123@host.zone.com"));
+        Assert.assertTrue(work.isEmail("89232335647@host.ea"));
+        Assert.assertTrue(work.isEmail("123@127.0.0.1"));
+        Assert.assertFalse(work.isEmail("Hello world"));
+        Assert.assertFalse(work.isEmail(""));
+        Assert.assertFalse(work.isEmail("@@@"));
+        Assert.assertFalse(work.isEmail("@"));
+        Assert.assertFalse(work.isEmail("12124.124124.ru"));
+    }
+
+    @Test
+    public void testKOrder() {
+        Assert.assertEquals(1, work.kOrderValue(new int[]{1, 0, 3}, 2));
+        Assert.assertEquals(5, work.kOrderValue(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, 5));
+        Assert.assertEquals(2, work.kOrderValue(new int[]{-1, -5, 9, 8, 1, 2, 5}, 4));
+    }
+
+    @Test
+    public void testDict() {
+        Assert.assertEquals(1,
+                work.countOfWordsFromDictionaryInString("mama mila ramu", new String[]{"mama"}));
+        Assert.assertEquals(4,
+                work.countOfWordsFromDictionaryInString("mama mila ramu, mama, mama!, MAMA", new String[]{"mama"}));
+        Assert.assertEquals(4,
+                work.countOfWordsFromDictionaryInString("a, b, c, d, e, f AA BB , A, B! C", new String[]{"a", "B"}));
+        Assert.assertEquals(5,
+                work.countOfWordsFromDictionaryInString("a, b, c, d, e, f AA BB , A, B! C", new String[]{"a", "B", "aa"}));
+    }
 }
